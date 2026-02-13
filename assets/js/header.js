306d4e59182544122fc25397aa21423ed5cbcbbf
scratch-technoval <232642260+scratch-technoval@users.noreply.github.com>
2026-02-13 20:47:37 +0100

    Modification de header.js pour les nouvelles pages

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,7 +3,19 @@ header = `
   <div class="header-inner">
     <div class="logo"><img src="./assets/favicon.ico" height="16" style="background-color: white; border-radius: 100%;" /> <a href="./index.html">five_lives</a></div>
     <nav class="nav">
-      <a href="./SSSS.html" role="menuitem">Encodage SSSS</a>
+      <a id="importance1" href="./search.html">Rechercher</a>
+      <a id="importance1" href="./newspaper.html">Journal</a>
+
+      <div id="importance-2" class="dropdown">
+        <a href="#" class="dropdown-toggle" aria-haspopup="true" aria-expanded="false">Cryptage ▾</a>
+        <div class="opener"></div>
+        <div class="dropdown-menu" role="menu" aria-label="Cryptage menu">
+          <a href="./SSSS.html" role="menuitem">Encodage SSSS</a>
+          <a href="./morse.html" role="menuitem">Morse</a>
+        </div>
+      </div>
+      <a id="importance-4" href="./edf-tempo.html">Couleur EDF Tempo</a>
+      </div>
     </nav>
   </div>
 </header>
